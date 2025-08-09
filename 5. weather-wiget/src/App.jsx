caeb345d695e35fcd80759affb95240106c051ff
YakovchukIvan <81960402+YakovchukIvan@users.noreply.github.com>
2025-08-09 23:17:21 +0300

    working with useEffect

---
+++ 5. weather-wiget/src/App.jsx
@@ -0,0 +1,57 @@
+import { useState, useEffect } from 'react';
+import './index.css';
+
+const KEY = 'f7def371145f43adbee94458250908';
+
+function App() {
+  const [city, setCity] = useState('Tenerife');
+  const [weatherData, setWeatherData] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function getData() {
+      try {
+        const res = await fetch(`http://api.weatherapi.com/v1/current.json?key=${KEY}&q=${city}`);
+
+        if (!res.ok) {
+          throw new Error(`${res.status}, ${res.statusText}`);
+        }
+
+        const data = await res.json();
+
+        setWeatherData(data);
+      } catch (err) {
+        console.log(err);
+        setError(err.message);
+      }
+    }
+    getData();
+  }, []);
+
+  return (
+    <div className="app">
+      <div className="widget-container">
+        <div className="weather-card-container">
+          <h1 className="app-title">{error ? error : 'Weather Widget'}</h1>
+          <div className="search-container">
+            <input type="text" placeholder="Enter city name" className="search-input" />
+          </div>
+        </div>
+        <div className="weather-card">
+          <h2>
+            {weatherData?.location?.name}, {weatherData?.location?.country}
+          </h2>
+          <img src="/vite.svg" alt="icon" className="weather-icon" />
+          <p className="temperature">11°C</p>
+          <p className="condition">rainy</p>
+          <div className="weather-details">
+            <p>Humidity: 20%</p>
+            <p>Wind: 22 km/h</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
